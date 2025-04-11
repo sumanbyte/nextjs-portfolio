c5a0a1a4b166392f8aaa349d02880a57fada6edd
Suman Parajuli <sumansupercell@gmail.com>
2025-04-11 17:19:58 +0545

    config update

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: isProd ? "/nextjs-portfolio" : "",
+  assetPrefix: isProd ? "/nextjs-portfolio" : "",
   output: isProd ? "export" : undefined,
 };
 
