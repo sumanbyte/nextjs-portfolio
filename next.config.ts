357b60114ccc9115108f69f724085a5d8eed38c0
Suman Parajuli <sumansupercell@gmail.com>
2025-04-11 18:21:29 +0545

    changes

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   basePath: isProd ? "/nextjs-portfolio" : "",
   assetPrefix: isProd ? "/nextjs-portfolio" : "",
   output: isProd ? "export" : undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
