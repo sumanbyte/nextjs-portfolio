debaa711a087a008e36b5199fa03c4667d565fc2
Suman Parajuli <sumansupercell@gmail.com>
2025-04-11 15:06:57 +0545

    test

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: process.env.NODE_ENV === "production" ? "export" : undefined,
 };
 
 export default nextConfig;
